1de36d201dabd84adc41b8a08e6335eae3208969
anthonydb <adebarros@gmail.com>
2015-12-22 16:58:37 -0500

    Pull footer into separate file to reduce redundant code.

--- index.php
+++ index.php
@@ -234,32 +234,4 @@
 
 </div><!-- / container-->
 
-<?php
-	/*-----------------------------------------------------------------------------------*/
-	/* Start Footer
-	/*-----------------------------------------------------------------------------------*/
-?>
-
-<footer class="site-footer" role="contentinfo">
-	<div class="site-info container">
-		<?php do_action( 'break_credits' ); ?>
-		<p>Copyright 2009-2016 Anthony DeBarros<br />
-		I'm <a href="https://github.com/anthonydb/Less">hacking</a> on the <a href="http://lessmade.com/themes/less" rel="theme">LESS</a> theme by <a href="http://jarederickson.com" rel="designer">Jared Erickson</a>.
-		Hosting is by <a href="http://www.webfaction.com/signup?affiliate=adebarros" >WebFaction</a>. </p>
-	</div><!-- .site-info -->
-</footer><!-- #colophon .site-footer -->
-
-<?php wp_footer(); ?>
-
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-11819184-2");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-
-</body>
-</html>
+<?php get_footer(); ?>
