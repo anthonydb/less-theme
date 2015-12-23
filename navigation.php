b36e5921d1ec2b6c1c3939a9b58b35b0c5224410
anthonydb <adebarros@gmail.com>
2015-12-22 22:57:57 -0500

    Reference nav links from a separate file.

---
+++ navigation.php
@@ -0,0 +1,25 @@
+<nav role="navigation" class="site-navigation main-navigation">
+    <ul>
+        <li class="page_item page-item-2">
+            <a href="http://www.anthonydebarros.com/about/">About</a>
+        </li>
+        <li class="page_item page-item-2265">
+            <a href="http://www.anthonydebarros.com/awards/">Awards</a>
+        </li>
+        <li class="page_item">
+            <a href="http://www.anthonydebarros.com/index/">Index</a>
+        </li>
+        <li class="page_item">
+            <a href="http://www.github.com/anthonydb/">GitHub</a>
+        </li>
+        <li class="page_item">
+            <a href="http://www.anthonydb.com">Sandbox</a>
+        </li>
+        <li class="page_item page-item-9">
+            <a href="http://www.anthonydebarros.com/clips/">Clips</a>
+        </li>
+        <li class="page_item page-item-526">
+            <a href="http://www.anthonydebarros.com/contact/">Contact</a>
+        </li>
+    </ul>
+</nav><!-- .site-navigation .main-navigation -->
