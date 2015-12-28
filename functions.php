7c0685947dd9fca1dd1cf50b736d494da4243a23
anthonydb <adebarros@gmail.com>
2015-12-27 21:19:58 -0500

    Whitespace.

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ add_theme_support( 'automatic-feed-links' );
 /*-----------------------------------------------------------------------------------*/
 /* register main menu
 /*-----------------------------------------------------------------------------------*/
-/*register_nav_menus( 
+/*register_nav_menus(
 	array(
 		'primary'	=>	__( 'Primary Menu', 'less' ),
 	)
@@ -22,17 +22,17 @@ add_theme_support( 'automatic-feed-links' );
 /* Enque Styles and Scripts
 /*-----------------------------------------------------------------------------------*/
 
-function less_scripts()  { 
+function less_scripts()  {
 
 	// theme styles
 	wp_enqueue_style( 'less-style', get_template_directory_uri() . '/style.css', '10000', 'all' );
-			
+
 	// add fitvid
 	wp_enqueue_script( 'less-fitvid', get_template_directory_uri() . '/js/jquery.fitvids.js', array( 'jquery' ), LESS_VERSION, true );
-	
+
 	// add theme scripts
 	wp_enqueue_script( 'less', get_template_directory_uri() . '/js/theme.min.js', array(), LESS_VERSION, true );
-  
+
 }
 add_action( 'wp_enqueue_scripts', 'less_scripts' );
 
