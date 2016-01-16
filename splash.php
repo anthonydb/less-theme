8babf49d2b393f35c2700dbfcb80e891f6e3ab53
anthonydb <adebarros@gmail.com>
2016-01-16 10:44:37 -0500

    Style block quotes.

--- splash.php
+++ splash.php
@@ -38,8 +38,8 @@ Template Name: Splash
 
         <div id="content" role="main">
         <article class="post">
-        <h1 class="splash-text">Hi, I'm <a href="http://www.anthonydebarros.com/about/">Anthony DeBarros</a>. I help teams make excellent <a href="http://www.anthonydebarros.com/awards/">digital products</a>. I've also been known to <a href="https://source.opennews.org/en-US/learning/understanding-households-and-relationships-census-/">analyze
-        data</a> and <a href="https://www.github.com/anthonydb">write code</a>. Right now, I direct product at <a href="https://www.documentcloud.org">DocumentCloud</a>. Please say <a href="http://www.anthonydebarros.com/contact/">hello</a>.</h1>
+        <h1 class="splash-text">Hi, I'm <a href="http://www.anthonydebarros.com/about/">Anthony DeBarros</a>. I help teams make excellent <a href="http://www.anthonydebarros.com/awards/">digital products</a>. I also <a href="https://source.opennews.org/en-US/learning/understanding-households-and-relationships-census-/">analyze
+        data</a> and <a href="https://www.github.com/anthonydb">write code</a>. Right now, I direct product development at <a href="https://www.documentcloud.org">DocumentCloud</a>. Please <a href="http://www.anthonydebarros.com/contact/">get in touch</a>.</h1>
 
         <h1 class="splash-hed">Recent writing:</h1>
         <?php query_posts('post_type=post&post_status=publish&posts_per_page=2&paged='. get_query_var('paged')); ?>
