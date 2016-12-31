a3840493c0331e42c0293b4b70a2f01ad88ea307
anthonydb <adebarros@gmail.com>
2016-12-30 21:51:41 -0500

    Revise splash page.

--- splash.php
+++ splash.php
@@ -38,20 +38,13 @@ Template Name: Splash
 
         <div id="content" role="main">
         <article class="post">
-        <h1 class="splash-text">Hi, I'm <a href="http://www.anthonydebarros.com/about/">Anthony DeBarros</a>. I help teams make excellent <a href="http://www.anthonydebarros.com/awards/">digital products</a>. I also <a href="https://source.opennews.org/en-US/learning/understanding-households-and-relationships-census-/">analyze
-        data</a> and <a href="https://www.github.com/anthonydb">write code</a>. Right now, I'm focused on journalism at <a href="http://www.questex.com/">Questex Media</a>. Please <a href="http://www.anthonydebarros.com/contact/">get in touch</a>.</h1>
-
-        <h1 class="splash-hed">Recent writing:</h1>
-        <?php query_posts('post_type=post&post_status=publish&posts_per_page=2&paged='. get_query_var('paged')); ?>
-        <?php if (have_posts()) : while (have_posts()) : the_post();?>
-        <div class="front-post" style="text-align: center;" id="post-<?php the_ID(); ?>">
-            <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title();?></a>
-            <span style="text-decoration: none; font-size: 12px; color: #96A08D";> <?php the_time('m.d.Y'); ?> | <?php comments_number(' 0 c', ' 1 c', ' % c'); ?></span></h2>
-        </div>
-        <?php endwhile; endif; wp_reset_query(); ?>
-        <div class="front-post" style="text-align: center;"><h2 style="text-align: center;"><a href="http://www.anthonydebarros.com/index/">Read more ...</a></h2></div>
-
-                </article>
+            <h1 class="splash-text">Anthony DeBarros</h1>
+            <div id="button-block">
+                <a href="http://www.anthonydebarros.com/about/"><button class="button button1">About</button></a>
+                <a href="http://www.anthonydebarros.com/index/"><button class="button button1">Writing</button></a>
+                <a href="http://www.anthonydebarros.com/contact/"><button class="button button1">Contact</button></a>
+            </div>
+        </article>
         </div><!-- #content .site-content -->
 
     </div><!-- #primary .content-area -->
@@ -66,7 +59,6 @@ Template Name: Splash
 <footer class="site-footer" role="contentinfo">
     <div class="site-info container">
         <?php do_action( 'break_credits' ); ?>
-        <p>Copyright 2009-2017 Anthony DeBarros</p>
     </div><!-- .site-info -->
 </footer><!-- #colophon .site-footer -->
 
