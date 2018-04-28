698f10c881566976d071916524b929584ec35a07
anthonydb <adebarros@gmail.com>
2018-04-28 15:27:48 -0400

    Revisions ahead of book on-sale

--- navigation.php
+++ navigation.php
@@ -1,22 +1,19 @@
 <nav role="navigation" class="site-navigation main-navigation">
     <ul>
+    <li class="page_item page-item-526">
+            <a href="/contact/">Contact</a>
+        </li>
+        <li class="page_item">
+            <a href="/index/">Posts</a>
+        </li>
+        <li class="page_item">
+            <a href="http://www.github.com/anthonydb/" target="_blank">GitHub</a>
+        </li>
         <li class="page_item page-item-2">
             <a href="/about/">About</a>
         </li>
-        <li class="page_item">
-            <a href="/index/">Writing</a>
-        </li>
         <li class="page_item page-item-2265">
-            <a href="/awards/">Awards</a>
-        </li>
-        <li class="page_item">
-            <a href="http://www.github.com/anthonydb/" target="_blank">Code</a>
-        </li>
-        <li class="page_item page-item-9">
-            <a href="/clips/">Clips</a>
-        </li>
-        <li class="page_item page-item-526">
-            <a href="/contact/">Contact</a>
+            <a href="/practical-sql/">Practical SQL Book</a>
         </li>
     </ul>
 </nav><!-- .site-navigation .main-navigation -->
