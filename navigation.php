d384f1af9214c82c5e6a5e97885eb7a92d3cbc2c
anthonydb <adebarros@gmail.com>
2018-05-02 22:56:29 -0400

    Update fonts and nav based on the kind recommendations of @reefdog

--- navigation.php
+++ navigation.php
@@ -1,19 +1,19 @@
 <nav role="navigation" class="site-navigation main-navigation">
     <ul>
-    <li class="page_item page-item-526">
-            <a href="/contact/">Contact</a>
-        </li>
+        <li class="page_item page-item-2265">
+            <a href="/practical-sql/">Book</a>
+        </li> 
         <li class="page_item">
-            <a href="/blog/">Posts</a>
-        </li>
-        <li class="page_item">
-            <a href="http://www.github.com/anthonydb/" target="_blank">GitHub</a>
+            <a href="/blog/">Blog</a>
         </li>
         <li class="page_item page-item-2">
             <a href="/about/">About</a>
         </li>
-        <li class="page_item page-item-2265">
-            <a href="/practical-sql/">Practical SQL Book</a>
+        <li class="page_item">
+            <a href="http://www.github.com/anthonydb/" target="_blank">Code</a>
+        </li>
+        <li class="page_item page-item-526">
+            <a href="/contact/">Contact</a>
         </li>
     </ul>
 </nav><!-- .site-navigation .main-navigation -->
