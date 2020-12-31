509ee4b393b6f298f64dbe040834f2036b4b78da
anthonydb <anthonymdebarros@gmail.com>
2020-12-31 14:42:12 -0500

    Happy New Year

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 <footer class="site-footer" role="contentinfo">
     <div class="site-info container">
         <?php do_action( 'break_credits' ); ?>
-        <p>Copyright 2009-2020 Anthony DeBarros<br />
+        <p>Copyright 2009-2021 Anthony DeBarros<br />
         Hosted by <a href="https://my.opalstack.com/signup?lmref=MJKmdw">Opalstack</a><br />
         <a href="/privacy-policy">Privacy Policy</a></p>
     </div><!-- .site-info -->
